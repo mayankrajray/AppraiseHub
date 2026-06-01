6b662c906c2a6c5c44cf22116674d0aadc78dab5
Mayank Raj <mayankrajray2006@gmail.com>
2026-06-01 14:22:14 +0530

    Phase 2: Added ResponseEntity and HTTP Status Codes

--- src/main/java/com/appraisehub/controller/EmployeeController.java
+++ src/main/java/com/appraisehub/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import com.appraisehub.model.Employee;
 import com.appraisehub.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.batch.BatchTransactionManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +19,39 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping
-    public List<Employee> getAllEmployees(){
-        return employeeService.getAllEmployees();
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+
     }
 
     @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable Integer id){
-        return employeeService.getEmployeeById(id);
+    public ResponseEntity<Employee> getEmployeeById (@PathVariable Integer id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if(employee !=null){
+            return ResponseEntity.ok(employee);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public Employee addEmployee(@RequestBody Employee employee){
-        return employeeService.addEmployee(employee);
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee savedEmployee = employeeService.addEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployee);
     }
 
     @PutMapping("/{id}")
-    public Employee updateEmployee (@PathVariable Integer id, @RequestBody Employee employee){
-        return employeeService.updateEmployee(id,employee);
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Integer id, @RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(id,employee);
+        if(updatedEmployee != null){
+            return ResponseEntity.ok(updatedEmployee);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable Integer id){
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Integer id){
         employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
     }
 }
